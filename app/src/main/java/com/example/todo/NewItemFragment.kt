49021abb935825050c533d68f8e65bb2ffa9cb8b
Alexx Mitchell <alexxmitchell25@gmail.com>
2020-03-30 17:58:49 -0400

    working on new item fragment

---
+++ app/src/main/java/com/example/todo/NewItemFragment.kt
@@ -0,0 +1,28 @@
+package com.example.todo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+
+class NewItemFragment : Fragment() {
+    private lateinit var viewModel: ToDoViewModel
+    private lateinit var binding: NewItemFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewModel = ViewModelProviders.of(activity!!).get(ToDoViewModel::class.java)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_new_item, container)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
